b25d1695256412368dcdd2441e764cd18669eeeb
Edmundo0994 <esala.094@gmail.com>
2022-08-18 11:42:15 -0500

    Basic ERC721 deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,20 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const deploy = async() => {
+  // Obtain the address that is going to deploy the contract
+  const [deployer] = await ethers.getSigners();
 
-async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  console.log("Deploying contract with the account", deployer.address);
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Platzi!");
+  // Get the contract factory
+  const PlatziPunks = await ethers.getContractFactory("PlatziPunks");
+  // Deploy the contract
+  const deployed = await PlatziPunks.deploy();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Platzi punks is deployed at: ", deployed.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main()
+deploy()
   .then(() => process.exit(0))
-  .catch((error) => {
+  .catch(error => {
     console.error(error);
     process.exit(1);
-  });
+  })
